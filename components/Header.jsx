6cdd9dabceb41b2da2981360b1d8ddb235c3a1f7
suryanshgupta133@gmail.com <suryanshgupta133@gmail.com>
2024-04-22 13:04:03 +0530

    change header text

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <header className={`header-container ${darkMode ? "dark" : ""}`}>
         <div className="header-content">
           <h2 className="title">
-            <a href="/">Where in the world?</a>
+            <a href="/">Where in the world...?</a>
           </h2>
           <p
             className="theme-changer"
